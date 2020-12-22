ff3033defa678a60af5f9ef1b6b8009126281e8c
Colin Gourlay <gourlay.colin@abc.net.au>
2020-12-22 10:28:02 +1000

    Add alternative encoded props config, and support mount points

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './polyfills';
 import * as acto from '@abcnews/alternating-case-to-object';
+import { decode } from '@abcnews/base-36-props';
+import { getMountValue, selectMounts } from '@abcnews/mount-utils';
 import App from './components/App/App.svelte';
-
-const MARKER_NAME = 'panny';
 
 const configs = [];
 
@@ -14,8 +14,8 @@ function render() {
 }
 
 function init() {
-  [...document.querySelectorAll(`a[name^="${MARKER_NAME}"]`)].forEach(markerEl => {
-    const blockEl = markerEl.closest('.Block');
+  selectMounts('panny').forEach(mountEl => {
+    const blockEl = mountEl.closest('.Block');
 
     if (!blockEl) {
       return;
@@ -27,33 +27,64 @@ function init() {
       return;
     }
 
-    const { id, start, end } = acto(markerEl.getAttribute('name').slice(MARKER_NAME.length));
+    /*
+      dataURL (and optional portraitDataURL) can be defined in the page in one
+      of two ways:
 
-    if (!id) {
+      1) An element with `data-odyssey-panny-{id}` and
+        `data-odyssey-panny-{id}-portrait` attributes, where {id} is the ID
+        prop on the #panny mount, e.g.:
+
+          <div data-mount id="pannyIDxyz"></div>
+          ...
+          <div
+            data-odyssey-panny-xyz="{...}/landscape/data.json"
+            data-odyssey-panny-xyz-portrait="{...}/portrait/data.json"
+          ></div>
+
+      2) A Base36 encoded object with dataURL & portraitDataURL props as the
+        ENCODED prop on the #panny mount, e.g.:
+
+          <div data-mount id="pannyENCODEDhfjghfds79ygfh3rgfhjgufds0g..."></div>
+
+    */
+
+    const { id, encoded, start, end } = acto(getMountValue(mountEl));
+
+    if (!id && !encoded) {
       return;
     }
 
-    const dataAttr = `data-odyssey-panny-${id}`;
+    let dataURL;
+    let portraitDataURL;
 
-    const dataEl = document.querySelector(`[${dataAttr}]`);
+    if (encoded) {
+      try {
+        ({ dataURL, portraitDataURL } = decode(encoded));
+      } catch (err) {
+        return console.error(err);
+      }
+    } else {
+      const dataAttr = `data-odyssey-panny-${id}`;
+      const dataEl = document.querySelector(`[${dataAttr}]`);
 
-    if (!dataEl) {
-      return;
+      if (!dataEl) {
+        return;
+      }
+
+      dataURL = dataEl.getAttribute(dataAttr);
+      portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
     }
-
-    let dataURL = dataEl.getAttribute(dataAttr);
 
     if (!dataURL) {
       return;
     }
 
-    const portraitDataURL = dataEl.getAttribute(`${dataAttr}-portrait`);
-
     if (portraitDataURL && window.innerWidth / window.innerHeight <= 0.75) {
       dataURL = portraitDataURL;
     }
 
-    markerEl.parentElement.parentElement.removeChild(markerEl.parentElement);
+    mountEl.parentElement.parentElement.removeChild(mountEl.parentElement);
 
     configs.push({
       rootEl,
